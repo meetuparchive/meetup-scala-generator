67eefc34efbdc8080eb7509107eb2fe5f7f1e266
Chris Lewis <chrisl@meetup.com>
2016-12-06 17:41:32 -0500

    rely on json4s-java-time for java.time (de)serialization

--- test.sh
+++ test.sh
@@ -48,7 +48,9 @@ export CP=$A_PATH && $CODEGEN generate -i swagger.yaml -l $GEN_NAME -o $GEN_DIR 
 # via compilation.
 
 if [ "$GEN_ARG" = "client" ]; then
-  echo 'libraryDependencies ++= Seq("org.json4s" %% "json4s-native" % "3.4.0", "com.squareup.okhttp3" % "okhttp" % "3.5.0")' > $GEN_DIR/build.sbt
+  echo 'scalaVersion := "2.11.8"
+  
+  libraryDependencies ++= Seq("org.json4s" %% "json4s-native" % "3.4.0", "com.squareup.okhttp3" % "okhttp" % "3.5.0", "com.meetup" %% "json4s-java-time" % "0.0.6")' > $GEN_DIR/build.sbt  
 fi
 
 echo "  -> Attempting to build the generated code ..."
