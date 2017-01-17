361e45f01473640372a354c214d5e9e0facc0740
Chris Lewis <chrisl@meetup.com>
2017-01-17 16:51:46 -0500

    parameterize swagger input file

--- test-all.sh
+++ test-all.sh
@@ -3,10 +3,12 @@
 # Test both the client and server generators regardless if one fails. If either
 # fails, exit with that exit code.
 
-./test.sh client
+SWAGGER_SPEC=$1
+
+./test.sh client $SWAGGER_SPEC
 R_CLIENT=$?
 
-./test.sh server
+./test.sh server $SWAGGER_SPEC
 R_SERVER=$?
 
 if [ $R_CLIENT -ne 0 ]; then
